dc482e34baea3e709869d7c7c8e23bd72546c12a
Bunny06022001 <Srinu9493184495@gmail.com>
2026-01-29 17:00:47 +0530

    Refactor dockerBuildPush to accept a configuration map

--- vars/dockerBuildPush.groovy
+++ vars/dockerBuildPush.groovy
@@ -1,1 +1,25 @@
-def call(string )
+def call(Map configure = [:] ){
+  String image = configure.image
+  String tag = configure.tag ?: 'latest'
+
+
+sh '''
+docker rm -f $(docker ps -aq) || true
+docker rmi -f $(docker images -q) || true
+docker build -t image:tag
+
+'''
+  withCredentials([
+      usernamePassword(
+      usernameVariable: 'DOCKER_USER',
+        passwordVariable: 'DOCKER_PASS'
+    )
+    
+  ]){
+    sh '''
+    echo ${DOCKER_PASS} | docker login -u ${DOCKER_USER} --password-stdin
+    docker push image:tag
+    '''
+  }
+
+}
