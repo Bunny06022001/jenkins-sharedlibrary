da64c5c44dbacb1648850b5147b955c12120aeb7
Bunny06022001 <Srinu9493184495@gmail.com>
2026-01-30 07:38:03 +0530

    Update dockerdeploy.groovy

--- vars/dockerdeploy.groovy
+++ vars/dockerdeploy.groovy
@@ -2,14 +2,14 @@ def call(Map configure = [:]){
 String image = configure.image
   String tag =  configure.tag ?: 'latest'
 
-if(params.ENV == prod){
+if(params.ENV == 'prod'){
   input(
     message: "are u sure to launch",
     ok: "launch now",
     submitter: "admin"
   )
 sh """
-docker run -d ---name=app:${tag} p \${APP_PORT}:8080 ${image}:${tag}
+docker run -d --name=app:${tag} -p \${APP_PORT}:8080 ${image}:${tag}
 """
 }
   
