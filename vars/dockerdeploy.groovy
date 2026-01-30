58a3257a6da73a208ec1eda600852ce73563de41
Bunny06022001 <Srinu9493184495@gmail.com>
2026-01-30 07:01:37 +0530

    Create dockerdeploy.groovy

---
+++ vars/dockerdeploy.groovy
@@ -0,0 +1,16 @@
+def call(Map configure = [:]){
+String image = configure.image
+  String tag =  configure.tag ?: 'latest'
+
+if(params.ENV == prod){
+  input(
+    message: "are u sure to launch",
+    ok: "launch now",
+    submitter: "admin"
+  )
+sh """
+docker run -d ---name=app:${tag} p \${APP_PORT}:8080 ${image}:${tag}
+"""
+}
+  
+}
