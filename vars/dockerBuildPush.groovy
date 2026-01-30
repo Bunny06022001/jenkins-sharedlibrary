76c704506ba0883fe18ae3ec0f019f14150b488d
Bunny06022001 <Srinu9493184495@gmail.com>
2026-01-30 11:39:44 +0530

    Update dockerBuildPush.groovy

--- vars/dockerBuildPush.groovy
+++ vars/dockerBuildPush.groovy
@@ -1,26 +1,4 @@
+import com.example.jenkins.DockerBuildPush
 def call(Map config = [:] ){
-  String image = config.image
-  String tag = config.tag ?: 'latest'
-
-
-sh """
-docker rm -f \$(docker ps -aq) || true
-docker rmi -f \$(docker images -q) || true
-docker build -t ${image}:${tag} .
-
-"""
-  withCredentials([
-      usernamePassword(
-        credentialsId: 'docker',
-      usernameVariable: 'DOCKER_USER',
-        passwordVariable: 'DOCKER_PASS'
-    )
-    
-  ]){
-    sh """
-    echo \${DOCKER_PASS} | docker login -u \${DOCKER_USER} --password-stdin
-    docker push ${image}:${tag}
-    """
-  }
-
+  new DockerBuildPush(this,config).buildPush()
 }
